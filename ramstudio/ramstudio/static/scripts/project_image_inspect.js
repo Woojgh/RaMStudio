0f7de02e664c5c01773ce6f08f1292a37f1f95d2
Woojgh <jamessalamonsen@gmail.com>
2018-02-15 02:15:08 -0800

    got things on project all woring

--- ramstudio/ramstudio/static/scripts/project_image_inspect.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-$(document).ready(function () {
-
-    $('.project-photo').click(function () {
-        $('#project-selected-image').attr('src', '{{ project.photos.first }}');
-    });
-});
