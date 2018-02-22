f8136e65ff0432668afa1c9053acc07a6d9a9fb4
woojgh <jamessalamonsen@gmail.com>
2018-02-21 16:23:10 -0800

    adding more styling and buyttons to grounds map page

--- ramstudio/grounds/urls.py
+++ ramstudio/grounds/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GroundsView
+from .views import GroundsView, GroundsShopView
 from . import views
 
 urlpatterns = [
     path('', views.GroundsView, name='grounds'),
+    path('shop/', views.GroundsShopView, name='grounds-shop'),
 ]
