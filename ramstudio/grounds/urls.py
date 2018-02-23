88f85ca0b9c846d399f6825df7a7d64837d31c2f
Woojgh <jamessalamonsen@gmail.com>
2018-02-22 16:56:45 -0800

    finishing up grounds

--- ramstudio/grounds/urls.py
+++ ramstudio/grounds/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import GroundsView, GroundsShopView
+from .views import GroundsView, GroundsShopView, GroundsDeckView, GroundsFirepitView, GroundsFrontView, GroundsGreenhouseView, GroundsHastaView, GroundsMeditationView, GroundsPondView, GroundsZenView
 from . import views
 
 urlpatterns = [
     path('', views.GroundsView, name='grounds'),
+    path('deck/', views.GroundsDeckView, name='grounds-deck'),
+    path('firepit/', views.GroundsFirepitView, name='grounds-firepit'),
+    path('front/', views.GroundsFrontView, name='grounds-front'),
+    path('greenhouse/', views.GroundsGreenhouseView, name='grounds-greenhouse'),
+    path('hasta/', views.GroundsHastaView, name='grounds-hasta'),
+    path('meditation/', views.GroundsMeditationView, name='grounds-meditation'),
+    path('pond/', views.GroundsPondView, name='grounds-pond'),
     path('shop/', views.GroundsShopView, name='grounds-shop'),
+    path('zen/', views.GroundsZenView, name='grounds-zen'),
 ]
--- ramstudio/grounds/views.py
+++ ramstudio/grounds/views.py
@@ -7,3 +7,35 @@ def GroundsView(request):
 
 def GroundsShopView(request):
     return render(request, 'grounds-shop.html')
+
+
+def GroundsDeckView(request):
+    return render(request, 'grounds-deck.html')
+
+
+def GroundsFirepitView(request):
+    return render(request, 'grounds-firepit.html')
+
+
+def GroundsFrontView(request):
+    return render(request, 'grounds-front.html')
+
+
+def GroundsGreenhouseView(request):
+    return render(request, 'grounds-greenhouse.html')
+
+
+def GroundsHastaView(request):
+    return render(request, 'grounds-hasta.html')
+
+
+def GroundsMeditationView(request):
+    return render(request, 'grounds-meditation.html')
+
+
+def GroundsPondView(request):
+    return render(request, 'grounds-pond.html')
+
+
+def GroundsZenView(request):
+    return render(request, 'grounds-zen.html')
